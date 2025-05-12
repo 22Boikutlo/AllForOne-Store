61b09582e9411532dc42cdd30840e511ccbef4f9
22Boikutlo <boikutlos@gmail.com>
2025-05-12 13:38:07 +0200

    Add project files.

---
+++ Data/AppDbContext.cs
@@ -0,0 +1,13 @@
+﻿using AllForOne_Store.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace AllForOne_Store.Data
+{
+    public class AppDbContext: IdentityDbContext<Users>
+    {
+         public AppDbContext(DbContextOptions options) : base(options) 
+        {
+        }
+    }
+}
---
+++ ViewModels/LogInViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AllForOne_Store.ViewModels
+{
+    public class LogInViewModel
+    {
+        [Required(ErrorMessage = "Email required.")]
+        [EmailAddress]
+        public string Email { get; set; }
+        [Required(ErrorMessage = "Password required.")]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+        [Display(Name = "Remember me?")]
+        public bool RememberMe { get; set; }
+    }
+}
